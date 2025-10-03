7fccae5fc4c33e141aa292986341d06e7401944d
TheEnderCore <theendercore@gmail.com>
2025-10-03 13:44:04 +0300

    langs

---
+++ src/client/kotlin/org/teamvoided/template/client/toolitp/Langs.kt
@@ -0,0 +1,6 @@
+package org.teamvoided.template.client.toolitp
+
+const val ENCHANTMENTS = "item.enders_improved_tooltips.enchantments"
+const val SHAPE = "item.enders_improved_tooltips.shape"
+const val COLORS = "item.enders_improved_tooltips.colors"
+const val EFFECTS = "item.enders_improved_tooltips.effects"
