7d225025c52db0c8a4b4f894f89483701f7b6fee
TheEnderCore <theendercore@gmail.com>
2025-10-03 13:44:48 +0300

    .

--- src/main/kotlin/org/teamvoided/template/Template.kt
+++ src/main/kotlin/org/teamvoided/template/Template.kt
@@ -1,21 +1,15 @@
 package org.teamvoided.template
 
-import me.fzzyhmstrs.fzzy_config.api.ConfigApi
 import net.minecraft.resources.ResourceLocation
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.teamvoided.template.config.TemplateConfig
 
 @Suppress("unused")
 object Template {
-    const val MODID = "template"
+    const val MODID = "enders_improved_tooltips"
 
     @JvmField
     val log: Logger = LoggerFactory.getLogger(Template::class.simpleName)
-
-    @JvmField
-    var config = ConfigApi.registerAndLoadConfig(::TemplateConfig)
-
     fun init() {
         log.info("Hello from Common")
     }
